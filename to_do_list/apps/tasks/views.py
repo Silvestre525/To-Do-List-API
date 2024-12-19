88ceb2f874b7240d6b5ef950506f9e2b87a17194
Silvestre525 <silvestrevillar02@gmail.com>
2024-12-19 16:52:44 -0300

    Modify: update

--- to_do_list/apps/tasks/views.py
+++ to_do_list/apps/tasks/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Task
+from rest_framework import viewsets, status
+from .serializers import TaskSerializer
 
-# Create your views here.
+class TaskViewSet(viewsets.ModelViewSet):
+    serializer_class = TaskSerializer
+    queryset = Task.objects.all()

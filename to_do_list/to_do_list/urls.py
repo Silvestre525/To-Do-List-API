52ef4fe93e13c9e22ee89cde35188b0320c74fab
Silvestre525 <silvestrevillar02@gmail.com>
2024-12-19 17:16:10 -0300

    update

--- to_do_list/to_do_list/urls.py
+++ to_do_list/to_do_list/urls.py
@@ -21,7 +21,7 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, Spec
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('tasks/', include('apps.tasks.urls')),
+    path('api/tasks/', include('apps.tasks.urls')),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     # Optional UI:
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
